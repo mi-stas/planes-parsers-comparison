d584815576b5e131dee10e492384df1f1f7d5617
mi-stas <36571012+mi-stas@users.noreply.github.com>
2022-12-11 10:01:09 +0300

    Add actual comparison

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.20"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://maven.openimaj.org")
+    }
+}
+
+dependencies {
+    implementation("com.soywiz.korlibs.korim:korim:3.3.1")
+    implementation("net.coobird:thumbnailator:0.4.18")
+    implementation("com.sksamuel.scrimage:scrimage-core:4.0.32")
+    implementation("org.openimaj:image-processing:1.3.10")
+    implementation("org.openpnp:opencv:4.5.5-1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
