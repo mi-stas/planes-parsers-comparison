da300f69ebe842a1509c642f2bef7d6b894a4a0d
mi-stas <36571012+mi-stas@users.noreply.github.com>
2023-01-02 17:58:53 +0300

    Delete src/main directory

--- src/main/kotlin/parsers/PlaneParser.kt
+++
@@ -1,7 +0,0 @@
-package parsers
-
-import structures.Point
-
-abstract class PlaneParser(val name: String) {
-    abstract fun parse(filePath: String): Map<Int, List<Point>>
-}
--- src/main/kotlin/utils.kt
+++
@@ -1,6 +0,0 @@
-const val BACKGROUND_COLOR_OFFSET = 16777216
-
-const val COLOR_BITS_NUMBER = 8
-
-fun convertSeparateToWholeRGB(r: Int, g: Int, b: Int): Int =
-    (r shl COLOR_BITS_NUMBER * 2) + (g shl COLOR_BITS_NUMBER) + b
